94cda5c8dc29c63d45b92af4a7752472fcf189a4
Peter Lan <peter.lan@smokeball.com>
2022-11-23 09:53:24 +1100

    update slack client

---
+++ src/AutoSlackEmoji.Core/Models/TokenRefreshResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AutoSlackEmoji.Core.Models
+{
+    public class TokenRefreshResponse
+    {
+        [JsonPropertyName("ok")]
+        public bool IsSuccessful { get; set; }
+        [JsonPropertyName("token")]
+        public string AccessToken { get; set; }
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; }
+        [JsonPropertyName("team_id")]
+        public string TeamId { get; set; }
+        [JsonPropertyName("user_id")]
+        public string UserId { get; set; }
+        [JsonPropertyName("iat")]
+        public int Iat { get; set; }
+        [JsonPropertyName("exp")]
+        public int Exp { get; set; }
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+    }
+}
--- src/AutoSlackEmoji.Core/SlackClient.cs
+++ src/AutoSlackEmoji.Core/SlackClient.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using AutoSlackEmoji.Core.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace AutoSlackEmoji.Core
@@ -13,21 +15,21 @@ namespace AutoSlackEmoji.Core
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _configuration;
-        private readonly string? _accessToken;
-        private readonly string? _refreshtoken;
+        private string? _accessToken;
+        private string? _refreshToken;
 
         public SlackClient(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _httpClientFactory = httpClientFactory;
             _configuration = configuration;
             _accessToken = _configuration["slack_access_token"];
-            _refreshtoken = _configuration["slack_refresh_token"];
+            _refreshToken = _configuration["slack_refresh_token"];
         }
 
         public async Task AddEmojiAsync(string emojiName, string emojiUrl)
         {
-            var client = _httpClientFactory.CreateClient();
-            var request = new HttpRequestMessage(HttpMethod.Post, "https://slack.com/api/emoji.add");
+            using var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://slack.com/api/emoji.add");
             var content = new FormUrlEncodedContent(new Dictionary<string, string>
             {
                 {"name", emojiName},
@@ -41,7 +43,22 @@ namespace AutoSlackEmoji.Core
 
         public async Task RefreshAccessTokenAsync()
         {
-            throw new NotImplementedException();
+            using var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://slack.com/api/tooling.tokens.rotate");
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                {"refresh_token", _refreshToken}
+            });
+            request.Content = content;
+
+            var result = await client.SendAsync(request);
+            var resultContent = await result.Content.ReadAsStringAsync();
+            var response = JsonSerializer.Deserialize<TokenRefreshResponse>(resultContent);
+            if (response != null)
+            {
+                _accessToken = response.AccessToken;
+                _refreshToken = response.RefreshToken;
+            }
         }
     }
 }
